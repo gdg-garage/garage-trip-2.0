d825702f87cd780f4e675e1cd8f480c8bab93a9a
Eva Mayerova <evca.mayerova@gmail.com>
2021-09-22 02:01:57 +0200

    mastermind init

--- escape/server/main.go
+++ escape/server/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
+
+var mastermind MasterMind = MasterMind{Solution: "RYPBRT"}
+
+type Page struct {
+	Title        string
+	Body         []byte
+	GuessHistory []GuessResult
+	Win          bool
+}
+
+func loadPage(title string) (*Page, error) {
+	return &Page{
+		Title:        title,
+		GuessHistory: mastermind.GuessHistory,
+		Win:          mastermind.Solved}, nil
+}
 
 func TextHandler(text string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +32,26 @@ func TextHandler(text string) func(http.ResponseWriter, *http.Request) {
 			log.Printf("Problem while handling %s %s\n", r.URL, err)
 		}
 	}
+}
+
+func MasterMindHandler(w http.ResponseWriter, r *http.Request) {
+	title := "Mastermind"
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Println(err.Error())
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("html/mastermind.html")
+	t.Execute(w, p)
+}
+
+func MasterMindGuessHandler(w http.ResponseWriter, r *http.Request) {
+	guess := r.FormValue("body")
+	_, _, err := mastermind.Guess(guess)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	http.Redirect(w, r, "/logik/game", http.StatusFound)
 }
 
 func main() {
@@ -28,7 +65,11 @@ func main() {
 	}
 	log.SetOutput(file)
 
-	http.HandleFunc("/axzfom", TextHandler("1A"))   // cross
-	http.HandleFunc("/PSAMRBJZ", TextHandler("2B")) // cars
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
+	http.HandleFunc("/axzfom", TextHandler("1A"))     // cross
+	http.HandleFunc("/PSAMRBJZ", TextHandler("2B"))   // cars
+	http.HandleFunc("/logik/game", MasterMindHandler) // mastermind
+	http.HandleFunc("/logik/guess", MasterMindGuessHandler)
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
